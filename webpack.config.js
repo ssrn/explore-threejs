09741dc26be0db4af3613bfeb94a4990129660b4
Svetlana Kondrateva <pe6i.is4o@gmail.com>
2023-04-17 18:20:51 +0300

    setup gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
